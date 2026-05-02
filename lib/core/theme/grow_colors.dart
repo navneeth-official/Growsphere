d146466e9ad6c92fc6a431a4e7484d7362cc2e6e
navneeth-official <jnavneethkrishnan@gmail.com>
2026-05-02 11:38:02 +0530

    Intial Commit

--- lib/core/theme/grow_colors.dart
+++ lib/core/theme/grow_colors.dart
@@ -8,7 +8,9 @@ abstract final class GrowColors {
   static const Color gray50 = Color(0xFFF9FAFB);
   static const Color gray100 = Color(0xFFF3F4F6);
   static const Color gray200 = Color(0xFFE5E7EB);
+  static const Color gray300 = Color(0xFFD1D5DB);
   static const Color gray400 = Color(0xFF9CA3AF);
+  static const Color gray500 = Color(0xFF6B7280);
   static const Color gray600 = Color(0xFF4B5563);
   static const Color gray700 = Color(0xFF374151);
 }
